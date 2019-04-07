f7a71c5e034d83862ab28e2a411461d925b06cf6
eetar1 <ekwan14@gmail.com>
2019-04-07 00:02:39 -0700

    Added whitenoise

--- ccfWorker/ccfWorker/settings.py
+++ ccfWorker/ccfWorker/settings.py
@@ -54,6 +54,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    
 ]
 
 MIDDLEWARE = [
@@ -64,6 +65,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 ROOT_URLCONF = "ccfWorker.urls"
