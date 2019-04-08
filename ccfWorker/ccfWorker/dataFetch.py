0261ea13a6da9cf9efe7f52de7e214279ebe37b7
Austin <smith.austin149@gmail.com>
2019-04-07 20:46:59 -0700

    Added data fetching.

---
+++ ccfWorker/ccfWorker/dataFetch.py
@@ -0,0 +1,30 @@
+import requests
+
+# Input: url to STAC catalog
+# Output: the list of urls to STAC items in the catalog
+def getSTACItemsFromCatalog(catalog):
+    resp = requests.get(catalog).json()["links"]
+    url = catalog.split("catalog.json")[0]
+
+    items = []
+    for elem in resp:
+        if elem["rel"] != "item":
+            continue
+        items.append(url + elem["href"])
+
+
+# Input: url to STAC item
+# Output: the url to the image
+def getImageURLFromSTACItem(item):
+    return requests.get(item).json()["assets"]["thumbnail"]["href"]
+
+
+# Used for testing purposes
+def main():
+    s = "https://cbers-stac-0-6.s3.amazonaws.com/CBERS4/MUX/065/094/catalog.json"
+    getSTACItemsFromCatalog(s)
+
+
+if __name__ == "__main__":
+    main()
+
